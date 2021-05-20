0c8336416c08da47ee44c8b96d1a9e49f99cda76
StasKat <stas_07@hotmail.com>
2021-05-20 08:56:34 -0700

    admin Login/registration

--- application/src/app/app-routing.module.ts
+++ application/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginPageComponent } from './admin/adminLogin/admin-login-page/admin-login-page.component';
+import { RegisterPageComponent } from './admin/adminLogin/admin-register-page/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"login",
+    component: AdminLoginPageComponent
+  },
+  {
+    path:"register",
+    component: RegisterPageComponent
+  },
+  {
+    path:"**",
+    redirectTo:"login"
+  }
+
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
 import { AdminLoginPageComponent } from './admin/adminLogin/admin-login-page/admin-login-page.component';
+import { RegisterPageComponent } from './admin/adminLogin/admin-register-page/register-page/register-page.component';
+import { LoginHeaderComponent } from './admin/adminLogin/loginHeader/login-header/login-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginPageComponent
+    AdminLoginPageComponent,
+    RegisterPageComponent,
+    LoginHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
